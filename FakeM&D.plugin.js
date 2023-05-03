1c05d7e81cb2ab101ba63dfc07b6cd782799627a
Shifts <74390871+ignshifts@users.noreply.github.com>
2023-05-02 20:17:39 -0400

    Update FakeM&D.plugin.js

--- FakeM&D.plugin.js
+++ FakeM&D.plugin.js
@@ -18,7 +18,7 @@ module.exports = class FakeMuteDeafen {
     WebSocket.prototype.send = function (data) {
       if (Object.prototype.toString.call(data) === "[object ArrayBuffer]") {
         if (text.decode(data).includes("self_deaf")) {
-          data = data.replace('"self_mute":false', "NahNotToday");
+          data = data.replace('"self_mute":false');
         }
       }
       WebSocket.prototype.original.apply(this, [data]);
